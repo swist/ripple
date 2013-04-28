f0dfbf8dd6f8a82ad9a4b0eb0af262dd4e21a60c
Tomasz Nguyen <me@swistofon.pl>
2013-04-28 06:21:38 +0100

    got rid of the pix

--- rippleapp/fb_friends.py
+++ rippleapp/fb_friends.py
@@ -15,16 +15,15 @@ def getFriends(token):
 	music_plays = tuple(int(item['id']) for item in user_graph.get_connections('me', 'music.listens', limit = 250)['data'])
 
 	query = {'friends': 'SELECT page_id, uid FROM page_fan WHERE page_id IN'+str(music_likes)+'AND uid IN (SELECT uid1 FROM friend WHERE uid2=me())',
-			 'friends_data': 'SELECT uid, name, pic_big, username FROM user WHERE uid IN (SELECT uid FROM'+'#friends)',
-			 'pages_data': 'SELECT name, page_id, pic_big, username FROM page WHERE page_id IN'+str(music_likes) }
+			 'friends_data': 'SELECT uid, name, username FROM user WHERE uid IN (SELECT uid FROM'+'#friends)',
+			 'pages_data': 'SELECT name, page_id, username FROM page WHERE page_id IN'+str(music_likes) }
 
 	query_results = user_graph.fql(query)
-
-	results = {}
-
+	print query_results
 	friends_data = (result for result in query_results if result['name'] == 'friends_data').next()['fql_result_set']
 	friends = (result for result in query_results if result['name'] == 'friends').next()['fql_result_set']
 	pages_data = (result for result in query_results if result['name'] == 'pages_data').next()['fql_result_set']
+
 
 	for friend in friends_data:
 		likes = [like['page_id'] for like in friends if like['uid'] == friend['uid']]
