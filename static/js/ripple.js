fca95d38b634f1981418e3eac1cf69109a163709
Jian Yuan <jianyuan@gmail.com>
2013-04-28 02:28:14 +0100

    Add user page

--- static/js/ripple.js
+++ static/js/ripple.js
@@ -18,12 +18,14 @@ $(document).ready(function() {
         // the user isn't logged in to Facebook.
       }
      });
+    Path.listen();
   });
   function refreshCurrentUser() {
     FB.api('/me', function(response) {
       fbUser = response;
-      console.log('Good to see you, ' + response.name + '.');
+      console.log('Good to see you, ' + response['name'] + '.');
       updateUserNavbar();
+      window.location.hash = '#/user/'+response['id'];
     });
   }
   function updateUserNavbar() {
@@ -31,14 +33,17 @@ $(document).ready(function() {
     if (fbUser) {
       $('.nav-auth li').show();
       $('.nav-auth .login').hide();
-      $('.nav-auth .me a').text(fbUser['name']);
+      $('.nav-auth .me a')
+        .attr('href', '#/user/'+fbUser['id'])
+        .text(fbUser['name']);
       $('.nav-auth .me-img img').attr('src', 'https://graph.facebook.com/'+fbUser['id']+'/picture');
     } else {
       $('.nav-auth li').hide();
       $('.nav-auth .login').show();
     }
   }
-  $('.login').click(function() {
+
+  $('.login a').click(function() {
     FB.login(function(response) {
       if (response.authResponse) {
         console.log('Welcome!  Fetching your information.... ');
@@ -48,4 +53,42 @@ $(document).ready(function() {
       }
     }, {scope: 'email,user_actions.music,user_events,user_interests,user_hometown,user_location,user_likes,user_videos,user_interests,user_actions.video,friends_about_me,friends_actions.music,friends_likes,friends_location,friends_hometown,friends_status'});
   });
+  $('.logout a').click(function() {
+    FB.logout(function(response) {
+      fbUser = undefined;
+      updateUserNavbar();
+    });
+  });
+
+  function notFound() {
+    console.error('page not found!');
+  }
+  function ensureLogin() {
+    if ( ! fbUser) {
+      window.location.hash = '';
+    }
+  }
+
+  var theContent = $('#the-content');
+  var userTpl = Handlebars.compile($('#user-template').html());
+
+  Path.map('#/user/:user_id').to(function() {
+    console.log('looking at ' + this.params['user_id']);
+    var user;
+    if (fbUser && fbUser['id'] == this.params['user_id']) {
+      user = fbUser;
+      theContent.html(userTpl(fbUser));
+    } else {
+      FB.api('/'+this.params['user_id'], function(response) {
+        theContent.html(userTpl(response));
+      });
+    }
+  });
+
+  Path.map('#/').to(function() {
+    console.log('home page');
+  });
+
+  Path.rescue(notFound);
+  Path.root('#/');
 });
